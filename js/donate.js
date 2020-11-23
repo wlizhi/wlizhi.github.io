d845928feb565bb47680665d5877c6ef0be37bb6
Eddie <2456767505@qq.com>
2020-11-23 12:42:32 +0800

    cc

--- js/donate.js
+++ js/donate.js
@@ -1,1 +1,35 @@
-window.onload=initDonate();function initDonate(){let tBei=document.createElement("div");let tOpo=document.createElement("div");tBei.id="t-bei";tBei.style.display="none";tBei.onclick=removeDonate;tOpo.id="t-opo";tOpo.style.display="none";let tOpoChild='<a href="javascript:void(0)" id="tca"><i class="fa fa-times"></i></a>';tOpoChild+='<div id="t-opo-title"><i class="fa fa-credit-card"></i> 打赏本站</div>';tOpoChild+='<div id="t-opo-content">';tOpoChild+='<img src="https://eddie-public-img.oss-cn-hangzhou.aliyuncs.com/blog/ali-pay-code.png">';tOpoChild+='<img src="https://eddie-public-img.oss-cn-hangzhou.aliyuncs.com/blog/wechat-pay-code.png">';tOpoChild+='<p>支付宝打赏</p>';tOpoChild+='<p>微信打赏</p>';tOpoChild+='</div>';tOpo.innerHTML=tOpoChild;let body=document.getElementsByTagName("body")[0];body.appendChild(tBei);body.appendChild(tOpo);document.getElementById("donate").onclick=donate;document.getElementById("tca").onclick=removeDonate;}function removeDonate(){document.getElementById('t-bei').style.display='none';document.getElementById('t-opo').style.display='none';}function donate(){document.getElementById('t-bei').style.display='block';document.getElementById('t-opo').style.display='block';}
+window.onload = initDonate();
+
+function initDonate() {
+    let tBei = document.createElement("div");
+    let tOpo = document.createElement("div");
+    tBei.id = "t-bei";
+    tBei.style.display = "none";
+    tBei.onclick = removeDonate;
+    tOpo.id = "t-opo";
+    tOpo.style.display = "none";
+    let tOpoChild = '<a href="javascript:void(0)" id="tca"><i class="fa fa-times"></i></a>';
+    tOpoChild += '<div id="t-opo-title"><i class="fa fa-credit-card"></i>&nbsp;&nbsp;请我喝杯咖啡吧~</div>';
+    tOpoChild += '<div id="t-opo-content">';
+    tOpoChild += '<img src="https://eddie-public-img.oss-cn-hangzhou.aliyuncs.com/blog/ali-pay-code.png">';
+    tOpoChild += '<img src="https://eddie-public-img.oss-cn-hangzhou.aliyuncs.com/blog/wechat-pay-code.png">';
+    tOpoChild += '<p>支付宝</p>';
+    tOpoChild += '<p>微信</p>';
+    tOpoChild += '</div>';
+    tOpo.innerHTML = tOpoChild;
+    let body = document.getElementsByTagName("body")[0];
+    body.appendChild(tBei);
+    body.appendChild(tOpo);
+    document.getElementById("donate").onclick = donate;
+    document.getElementById("tca").onclick = removeDonate;
+}
+
+function removeDonate() {
+    document.getElementById('t-bei').style.display = 'none';
+    document.getElementById('t-opo').style.display = 'none';
+}
+
+function donate() {
+    document.getElementById('t-bei').style.display = 'block';
+    document.getElementById('t-opo').style.display = 'block';
+}
